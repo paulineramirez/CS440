07cc12d2c08d4e80c3ed0653d6323ab4c347dac7
ninjadood7 <josephcho10@yahoo.com>
2017-04-05 19:14:53 -0400

    Dynammic and Static compiled together

--- PA2/balltrack.py
+++ PA2/balltrack.py
@@ -1,17 +1,37 @@
+from collections import deque
 import imutils
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
-from collections import deque
 
 # lower =  (123, 11, 41)
 # upper = (250, 168, 191)
 
 lower = (0, 48, 80)
 upper = (20, 255, 255)
+pts = deque(maxlen=32)
 
 
 cap = cv2.VideoCapture(0)
+
+def isWave(pts):
+	if list(pts)[0] is None: 
+		return False
+
+	start = pts[0]
+	end = pts[len(pts)-1]
+
+	if start is None or end is None:
+		return False
+
+	# check if ys in reasonable range with each other
+	if np.abs(start[1] - end[1]) in range(50):
+		# check distance formula to see if distance is greater than 400 pixels
+		if np.abs(start[0] - end[0]) in range(300):
+			return True
+
+	return False
+
 
 while(True):
 	# Capture frame-by-frame
@@ -53,14 +73,28 @@ while(True):
 			cv2.circle(frame, (int(x), int(y)), int(radius),
 			(0, 255, 255), 2)
 			cv2.circle(frame, center, 5, (0, 0, 255), -1)
-			cv2.putText(frame,'Motion Detected',(14,20),0,1,(255,255,255)) 
+			# cv2.putText(frame,'Motion Detected',(14,20),0,1,(255,255,255)) 
 
-	# # update the points queue
-	# pts.appendleft(center)
 
 	# show the frame to our screen
-	cv2.imshow("Frame", frame)
+	pts.appendleft(center)
+	# loop over the set of tracked points
+	for i in xrange(1, len(pts)):
+		# if either of the tracked points are None, ignore
+		# them
+		if pts[i - 1] is None or pts[i] is None:
+			continue
+		#print('CURRENT POINT: ' + str(pts[i]))
+ 
+		# otherwise, compute the thickness of the line and
+		# draw the connecting lines
+		thickness = int(np.sqrt(32 / float(i + 1)) * 2.5)
+		cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+ 	# print(pts)
+ 	if isWave(pts):
+ 		cv2.putText(frame,'Hello',(14,20),0,1,(255,255,255)) 
 
+	cv2.imshow("Frame", frame)
 
 	# if the 'q' key is pressed, stop the loop
 	if cv2.waitKey(1) & 0xFF == ord('q'):
--- PA2/test.py
+++ PA2/test.py
@@ -5,6 +5,7 @@ http://www.pyimagesearch.com/2014/08/04/opencv-python-color-detection/
 
 '''
 
+from collections import deque
 import imutils
 import numpy as np
 import cv2
@@ -12,6 +13,10 @@ from matplotlib import pyplot as plt
 
 lower = np.array([0, 48, 80], dtype = "uint8")
 upper = np.array([20, 255, 255], dtype = "uint8")
+
+# lower = (0, 48, 80)
+# upper = (20, 255, 255)
+pts = deque(maxlen=16)
 
 cap = cv2.VideoCapture(0)
 
@@ -23,12 +28,33 @@ def isThumbsUp(w,h):
     return False
 
 def isHeart(w,h):
-    if w in range(300, 370) and h in range(85, 140):
+    if w in range(300, 400) and h in range(55, 170):
         return True
     return False
 
 
+
+def isWave(pts):
+    if list(pts)[0] is None: 
+        return False
+
+    start = pts[0]
+    end = pts[len(pts)-1]
+
+    if start is None or end is None:
+        return False
+
+    # check if ys in reasonable range with each other
+    if np.abs(start[1] - end[1]) in range(50):
+        # check distance formula to see if distance is greater than 400 pixels
+        if np.abs(start[0] - end[0]) > 250:
+            return True
+
+    return False
+
+
 while(True):
+
     # Capture frame-by-frame
     ret, frame = cap.read()
 
@@ -36,15 +62,12 @@ while(True):
     if not ret:
     	break
 
-    frame = imutils.resize(frame, width=600)
+
+    # frame = imutils.resize(frame, width=600)
     converted = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-
-    #print(converted.shape)
-    #print(lower.shape)
-    #print(upper.shape)
+    # Start of static gesture
     skinMask = cv2.inRange(converted, lower, upper)
-    # # print(skinMask.shape)
 
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
     skinMask = cv2.erode(skinMask, kernel, iterations = 2)
@@ -53,14 +76,14 @@ while(True):
     skinMask = cv2.GaussianBlur(skinMask, (3, 3), 0)
 
     skin = cv2.bitwise_and(frame, frame, mask = skinMask)
-    cv2.imshow('frame2', skin)
+    # cv2.imshow('frame2', skin)
 
     skin = cv2.cvtColor(skin, cv2.COLOR_BGR2GRAY)
 
     #create a matrix of countours (triples of their boundaries)
     contours = cv2.findContours(skin, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
-    for c in contours[0]: #countours[1] contains the dimensions of detected object
+    for c in contours[1]: #countours[1] contains the dimensions of detected object
         rect = cv2.boundingRect(c) #find the bounds of detected object (as a rectangle)
         if rect[2] < 100 or rect[3] < 100: continue #JACKIE: might need to play with these
         #print cv2.contourArea(c)
@@ -73,14 +96,55 @@ while(True):
             cv2.putText(frame,'<3!',(x+w+10,y+h),0,3,(255,255,255)) 
         
         #creates rect & text around current frame
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,255,255),5)
-        # cv2.putText(frame,'Skin Detected',(x+w+10,y+h),0,3,(255,255,255))
+        # cv2.rectangle(frame,(x,y),(x+w,y+h),(255,255,255),5)
+    # Start of dynamic gesture
 
-    # Draw a blue line with thickness of 5 px
-    cv2.rectangle(skin,(15,20),(70,50),(0,255,0),5)
-    # print("here: " + str(contours[0]))
-    # print("there: " + str(contours[1]))
-    # print("joe pls: " + str(contours[2]))
+    # a series of dilations and erosions to remove any small
+    # blobs left in the mask
+    mask = cv2.inRange(converted, lower, upper)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
+
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+    cv2.CHAIN_APPROX_SIMPLE)[-2]
+    center = None
+
+    # only proceed if at least one contour was found
+    if len(cnts) > 0:
+        # find the largest contour in the mask, then use
+        # it to compute the minimum enclosing circle and
+        # centroid
+        c = max(cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+        # only proceed if the radius meets a minimum size
+        # if radius > 10:
+        #     # draw the circle and centroid on the frame,
+        #     # then update the list of tracked points
+        #     cv2.circle(frame, (int(x), int(y)), int(radius),
+        #     (0, 255, 255), 2)
+        #     cv2.circle(frame, center, 5, (0, 0, 255), -1)
+            # cv2.putText(frame,'Motion Detected',(14,20),0,1,(255,255,255)) 
+
+    # show the frame to our screen
+    pts.appendleft(center)
+    # loop over the set of tracked points
+    for i in xrange(1, len(pts)):
+        # if either of the tracked points are None, ignore
+        # them
+        if pts[i - 1] is None or pts[i] is None:
+            continue
+        #print('CURRENT POINT: ' + str(pts[i]))
+ 
+        # otherwise, compute the thickness of the line and
+        # draw the connecting lines
+        # thickness = int(np.sqrt(32 / float(i + 1)) * 2.5)
+        # cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+    # print(pts)
+    if isWave(pts):
+        cv2.putText(frame,'Hello',(100,100),0,5,(255,255,255)) 
     # Display the resulting frame
     cv2.imshow('frame', frame)
 
